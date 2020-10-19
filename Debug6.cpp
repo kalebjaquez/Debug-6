a290e1ad6eb9c229a465461d3fba593c7c2368fd
Kaleb <72029196+kalebjaquez@users.noreply.github.com>
2020-10-18 20:05:30 -0500

    Update Debug6.cpp

--- Debug6.cpp
+++ Debug6.cpp
@@ -16,14 +16,18 @@ meant to check if the code is actually in order.
 // Array that needs sorting
 int arr[] = {20,12,65,45,98,65,85,12,54,78,55,13,50,19,84,20,43,10};
 int arrSize =  sizeof(arr)/sizeof(arr[0]);
-int main() {
-
+int temp; 
+// made a int temp
+int main() { 
   //Bit of code for sorting
   for (unsigned j=arrSize;j>0;j--){
     for (unsigned i=1;i<j;i++){
       if (arr[i-1]>arr[i]){
         arr[i-1] = arr[i];
+        // added what temp equals
+        temp = arr[i];
         arr[i] = arr[i-1];
+        arr[i-1] = temp;
         }
     }
   }
